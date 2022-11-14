1f863323b958993fc757357c47975187cb797aaf
Max Chiu <k2dliaxri@hotmail.com>
2022-11-14 22:57:11 +0800

    add clip edit

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { ClipService } from 'src/app/services/clip.service';
 import IClip from 'src/app/models/clip.model';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-manage',
@@ -15,7 +16,8 @@ export class ManageComponent implements OnInit {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private clipService: ClipService
+    private clipService: ClipService,
+    private modal: ModalService
   ) { }
 
   ngOnInit(): void {
@@ -46,4 +48,9 @@ export class ManageComponent implements OnInit {
     })
   }
 
+  openModal($event: Event, clip: IClip) {
+    $event.preventDefault()
+
+    this.modal.toggleModal('editClip')
+  }
 }
